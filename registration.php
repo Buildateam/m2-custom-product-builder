3348aacdd1d7f49224cf7c4f0212a66874161d21
morozov.group <kir.morozov@gmail.com>
2017-07-14 16:07:49 +0000

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Buildateam_Productbuilder',
+    'Buildateam_CustomProductBuilder',
     __DIR__
 );

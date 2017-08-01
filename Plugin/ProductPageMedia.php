ee8c252bb9d0213a9876a544de46b2e630ca877c
Kirill Morozov <kir.morozov@gmail.com>
2017-08-01 14:22:44 +0300

    #2: Hiding components of product page to replace with product

---
+++ Plugin/ProductPageMedia.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Buildateam\CustomProductBuilder\Plugin;
+
+
+class ProductPageMedia
+{
+    /**
+     * @param \Magento\ProductVideo\Block\Product\View\Gallery|Magento\Catalog\Block\Product\View\Gallery $subject
+     * @param callable $proceed
+     * @return string
+     */
+    public function aroundToHtml($subject, callable $proceed)
+    {
+        $attribute = $subject->getProduct()->getData('json_config');
+        $result = "";
+        /* if (empty($attribute)) {
+              $result= $proceed();
+        }
+          */
+        return $result;
+    }
+
+}

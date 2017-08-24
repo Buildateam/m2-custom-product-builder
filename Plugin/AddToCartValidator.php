d73bb1ba5caae1cb763739e5b064c06214f47616
Kirill Morozov <kir.morozov@gmail.com>
2017-08-25 02:41:03 +0300

    #4: Initial addToCart, removed duplicate notification.

--- Plugin/AddToCartValidator.php
+++ Plugin/AddToCartValidator.php
@@ -6,6 +6,18 @@ namespace Buildateam\CustomProductBuilder\Plugin;
 class AddToCartValidator
 {
     /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $_checkoutSession;
+
+    public function __construct(
+        \Magento\Checkout\Model\Session $checkoutSession
+    )
+    {
+        $this->_checkoutSession = $checkoutSession;
+    }
+
+    /**
      * @param \Magento\ProductVideo\Block\Product\View\Gallery|Magento\Catalog\Block\Product\View\Gallery $subject
      * @param callable $proceed
      * @return string
@@ -13,6 +25,11 @@ class AddToCartValidator
     public function aroundValidate($subject, callable $proceed, $request)
     {
         if ($request->getHeader('X_CUSTOM_PRODUCT_BUILDER')) {
+            
+            $payload = json_decode(file_get_contents('php://input'),1);
+            $request->setParam('qty', $payload['qty']);
+
+            $this->_checkoutSession->setNoCartRedirect(true);
             return true;
         }
         return $proceed($request);

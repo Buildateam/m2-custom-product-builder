a2cafcc853a1aadc3690f5f1bbe0ce954208d6ce
Kirill Morozov <kir.morozov@gmail.com>
2017-08-25 00:00:30 +0300

    #4: Initial addToCart

---
+++ Plugin/AddToCartValidator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Buildateam\CustomProductBuilder\Plugin;
+
+
+class ProductPageHideBlock
+{
+    /**
+     * @param \Magento\ProductVideo\Block\Product\View\Gallery|Magento\Catalog\Block\Product\View\Gallery $subject
+     * @param callable $proceed
+     * @return string
+     */
+    public function aroundValidate($subject, callable $proceed, $request)
+    {
+        if ($request->getHeader('X_CUSTOM_PRODUCT_BUILDER')) {
+            return true;
+        }
+        return $proceed($request);
+    }
+
+}

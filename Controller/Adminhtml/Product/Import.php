58e7d85a3106f857c22367fd85850d704667e21d
Tedo <tz.gogichashvili@gmail.com>
2017-11-13 10:28:46 +0300

    [update] add copyright

--- Controller/Adminhtml/Product/Import.php
+++ Controller/Adminhtml/Product/Import.php
@@ -1,21 +1,25 @@
 <?php
 /**
- * Saraiva Platform | Fcamara
+ * Copyright © 2017 Indigo Geeks, Inc. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions
+ * are met:
  *
- * NOTICE OF LICENSE
  *
- * DISCLAIMER
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * All advertising materials mentioning features or use of this software must display the following acknowledgement:
+ * This product includes software developed by the the organization.
+ * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.fcamara.com.br for more information.
  *
- * @category  ${MAGENTO_MODULE_NAMESPACE}
- * @package   ${MAGENTO_MODULE_NAMESPACE}_${MAGENTO_MODULE}
- *
- * @copyright Copyright (c) 2017 Fcamara - Saraiva Platform (http://www.fcamara.com.br)
- *
- * @author    Tiago Daniel <tiago.daniel@fcamara.com.br>
+ * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDER "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 namespace Buildateam\CustomProductBuilder\Controller\Adminhtml\Product;

cc9d8f9209e5b8cf4d03f71b8550ae0a1151a03e
Tedo <tz.gogichashvili@gmail.com>
2018-08-29 13:29:52 +0300

    [fix] Email copy send fix

--- Model/Order/Email/SenderBuilder.php
+++ Model/Order/Email/SenderBuilder.php
@@ -1,35 +1,54 @@
 <?php
-
 namespace Buildateam\CustomProductBuilder\Model\Order\Email;
 
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Mail\Template\TransportBuilder;
 use Magento\Framework\Mail\Template\TransportBuilderByStore;
 use Magento\Sales\Model\Order\Email\Container\IdentityInterface;
 use Magento\Sales\Model\Order\Email\Container\Template;
 use Magento\Sales\Model\Order\Email\SenderBuilder as BaseSender;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Framework\Mail\MessageInterface;
 
 class SenderBuilder extends BaseSender
 {
     /**
-     * @var TransportBuilderByStore
+     * SenderBuilder constructor.
+     * @param Template $templateContainer
+     * @param IdentityInterface $identityContainer
+     * @param ObjectManagerInterface $objectManager
      */
-    private $transportBuilderByStore;
-
-    public function __construct(Template $templateContainer, IdentityInterface $identityContainer, TransportBuilder $transportBuilder, TransportBuilderByStore $transportBuilderByStore = null)
-    {
-        parent::__construct($templateContainer, $identityContainer, $transportBuilder, $transportBuilderByStore);
-        $this->transportBuilderByStore = $transportBuilderByStore ?: ObjectManager::getInstance()->get(
-            TransportBuilderByStore::class
+    public function __construct(
+        Template $templateContainer,
+        IdentityInterface $identityContainer,
+        ObjectManagerInterface $objectManager
+    ) {
+        /** @var MessageInterface $message */
+        $message = $objectManager->create(MessageInterface::class);
+        /** @var TransportBuilder $transportBuilder */
+        $transportBuilder = $objectManager->create(
+            TransportBuilder::class,
+            ["message" => $message]
         );
+        /** @var TransportBuilderByStore $transportBuilderByStore */
+        $transportBuilderByStore = $objectManager->create(
+            TransportBuilderByStore::class,
+            ["message" => $message]
+        );
+        parent::__construct($templateContainer, $identityContainer, $transportBuilder, $transportBuilderByStore);
     }
 
+    /**
+     * @throws \Magento\Framework\Exception\MailException
+     */
     public function send()
     {
         $this->sendToCustomer();
         $this->sendCopyWithAllOptions();
     }
 
+    /**
+     * @throws \Magento\Framework\Exception\MailException
+     */
     public function sendToCustomer()
     {
         $this->configureEmailTemplate();
@@ -44,15 +63,20 @@ class SenderBuilder extends BaseSender
         $transport->sendMessage();
     }
 
+    /**
+     * @throws \Magento\Framework\Exception\MailException
+     */
     public function sendCopyWithAllOptions()
     {
         $copyTo = $this->identityContainer->getEmailCopyTo();
 
         if (!empty($copyTo)) {
             foreach ($copyTo as $email) {
-                $this->transportBuilder->setTemplateIdentifier($this->templateContainer->getTemplateId());
-                $this->transportBuilder->setTemplateOptions($this->templateContainer->getTemplateOptions());
-                $this->transportBuilder->setTemplateVars($this->templateContainer->getTemplateVars());
+                $this->configureEmailTemplate();
+                $this->transportBuilder->setFrom(
+                    $this->identityContainer->getEmailIdentity(),
+                    $this->identityContainer->getStore()->getId()
+                );
                 $this->transportBuilder->addTo($email);
 
                 $transport = $this->transportBuilder->getTransport();

eaa6413d0c1c4e65b95c2af549b948b9c005434a
Alexey Eremenko <ae@buildateam.io>
2019-09-21 05:01:23 +0000

    adding error handling and error response http status code to the UploadImage.php when JSON is malformed

--- Controller/Product/UploadImage.php
+++ Controller/Product/UploadImage.php
@@ -1,94 +1,92 @@
-<?php
-/**
- * Copyright (c) 2017 Indigo Geeks, Inc. All rights reserved.
- *
- * General.
- * The custom product builder software and documentation accompanying this License
- * whether on disk, in read only memory, on any other media or in any other form (collectively
- * the “Software”) are licensed, not sold, to you by copyright holder, Indigo Geeks, Inc.
- * (“Buildateam”) for use only under the terms of this License, and Buildateam reserves all rights
- * not expressly granted to you. The rights granted herein are limited to Buildateam’s intellectual
- * property rights in the Buildateam Software and do not include any other patents or
- * intellectual property rights. You own the media on which the Buildateam Software is
- * recorded but Buildateam and/or Buildateam’s licensor(s) retain ownership of the Software
- * itself.
- *
- * Permitted License Uses and Restrictions.
- * This License allows you to install and use one (1) copy of the Software.
- * This License does not allow the Software to exist on more than one production domain.
- * Except as and only to the extent expressly permitted in this License or by applicable
- * law, you may not copy, decompile, reverse engineer, disassemble, attempt to derive
- * the source code of, modify, or create derivative works of the Software or any part
- * thereof. Any attempt to do so is a violation of the rights of Buildateam and its licensors of
- * the Software. If you breach this restriction, you may be subject to prosecution and
- * damages.
- *
- * Transfer.
- * You may not rent, lease, lend or sublicense the Software.
- *
- * Termination.
- * This License is effective until terminated. Your rights under this
- * License will terminate automatically without notice from Buildateam if you fail to comply
- * with any term(s) of this License. Upon the termination of this License, you shall cease
- * all use of the Buildateam Software and destroy all copies, full or partial, of the Buildateam
- * Software.
- *
- * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDER "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE SOFTWARE IS NOT INTENDED FOR USE IN WHICH THE FAILURE OF
- * THE SOFTWARE COULD LEAD TO DEATH, PERSONAL INJURY, OR SEVERE PHYSICAL OR ENVIRONMENTAL DAMAGE.
- */
-
 namespace Buildateam\CustomProductBuilder\Controller\Product;
 
-use \Buildateam\CustomProductBuilder\Helper\Data;
-use \Magento\Backend\App as AdminApp;
-use \Magento\Catalog\Model\ProductRepository;
-use \Magento\Framework\App\Action\Context;
-use \Magento\Framework\Filesystem;
-use \Magento\Framework\View\Result\PageFactory;
-use \Magento\Store\Model\StoreManagerInterface;
+use \Magento\Framework\Controller\ResultFactory;
 
-class UploadImage extends \Magento\Framework\App\Action\Action
+class Export extends \Magento\Framework\App\Action\Action
 {
-
-
     protected $_resultPageFactory;
     protected $_productRepository;
-    protected $_jsonProductContent;
-    protected $_auth;
-    protected $_fileSystem;
-    protected $_storeInterface;
-    protected $_helper;
+    protected $_jsonHelper;
 
     public function __construct(
-        Context $context,
-        PageFactory $resultFactory,
-        ProductRepository $productRepository,
-        Filesystem $fileSystem,
-        AdminApp\Action\Context $adminContext,
-        StoreManagerInterface $storeManager,
-        Data $helper
-    )
-    {
-        $this->_auth = $adminContext->getBackendUrl();
-        $this->_resultPageFactory = $resultFactory;
-        $this->_productRepository = $productRepository;
-        $this->_fileSystem = $fileSystem;
-        $this->_storeInterface = $storeManager;
-        $this->_helper = $helper;
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Json\Helper\Data $jsonHelper,
+        \Magento\Framework\View\Result\PageFactory $resultFactory,
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+    ) { 
+        $this->_jsonHelper          = $jsonHelper;
+        $this->_resultPageFactory   = $resultFactory;
+        $this->_productRepository   = $productRepository;
+        $this->resultRawFactory     = $resultRawFactory;
         parent::__construct($context);
     }
 
+    /**
+     * Get Json_Configuration from a specific product
+     * @return \Magento\Framework\Controller\Result\Raw
+     */
     public function execute()
     {
-        $imagePath = $this->_helper->uploadImage(file_get_contents('php://input'));
-        $imageBaseUrl = $this->_storeInterface->getStore()->getBaseUrl('media') . $imagePath;
-        $body = json_encode($imageBaseUrl);
-        $result = $this->resultFactory->create('raw');
-        $result->setHeader("Content-Type", 'application/json');
-        $result->setContents($body);
-
-        return $result;
-
+        $productId     = (int)$this->getRequest()->getParam('id', 0);
+        $product       = $this->_productRepository->getById($productId);
+        $productConfig = $product->getData('json_configuration');
+        if (!$productConfig) $productConfig = $this->_getBaseConfig($product);
+        $productConfigObject = json_decode($productConfig);
+        $resultRaw = $this->resultRawFactory->create();
+        $resultRaw->setHeader("Content-Type", 'application/json');
+        
+        if(!$productConfigObject){
+            $resultRaw->setHttpResponseCode(\Magento\Framework\Webapi\Exception::HTTP_INTERNAL_ERROR);     
+            $resultRaw->setContents(json_encode(['error_message' => 'ERROR PARSING PRODUCT CONFIG JSON']));
+        } else {
+            // SPACE SAVING: removing available fonts since em already present in cpb-frontend
+            $productConfigObject->settings->fonts->available = [];
+            $resultRaw->setContents(json_encode($productConfigObject));
+        }
+        return $resultRaw;
     }
 
+    protected function _getBaseConfig($product)
+    {   
+        $name = json_encode($product->getName());
+        $price = json_encode((float)$product->getPrice());
+        
+        return <<<JSON
+{ 
+  "settings": {
+    "isAdmin": true,
+    "theme": {
+      "id": "alpine-white"
+    },
+    "views": { 
+      "front": true,
+      "back": false,
+      "left": false,
+      "right": false,
+      "top": false,
+      "bottom": false
+    },
+    "currentView": "front",
+    "defaultView": "front",
+    "viewControls": "arrows",
+    "hasSummary": true,
+    "layout": "col-tabs",
+    "currency": "USD",
+    "cdnPath": "https://magento.thecustomproductbuilder.com/media/"
+  },
+  "data": {
+    "name": $name,
+    "base": {
+      "price": $price,
+      "image": {}
+    },
+    "panels": [],
+    "layers": [],
+    "price": null,
+    "isFetchingCategories": true
+  }
+}
+JSON;
+    }
 }

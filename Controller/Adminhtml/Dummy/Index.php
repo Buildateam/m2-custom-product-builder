e785cad193749f5dd0840a433538cb0f93f2a823
Maksim Ubeyko <mubenko@magenius.team>
2020-02-18 13:25:12 +0000

    [BUG] fix logs out when editing cpb (#buildateam/m2-custom-product-builder/issues/18)

---
+++ Controller/Adminhtml/Dummy/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Buildateam\CustomProductBuilder\Controller\Adminhtml\Dummy;
+
+use Magento\Backend\App\Action;
+
+/** Dummy controller, for checking session */
+class Index extends Action
+{
+    public function execute()
+    {
+    }
+}

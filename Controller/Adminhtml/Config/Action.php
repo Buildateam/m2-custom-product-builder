48820c24c3b3e8bdacb3c55b64bf6acb9e29e590
Tedo Gogichashvili <tgogichashvili@magenius.team>
2020-04-13 20:04:24 +0300

    Refactoring, remove custom jsonSerializer (#29)

--- Controller/Adminhtml/Config/Action.php
+++ Controller/Adminhtml/Config/Action.php
@@ -6,35 +6,54 @@ namespace Buildateam\CustomProductBuilder\Controller\Adminhtml\Config;
 
 use Magento\Backend\App\Action as BackendAction;
 use Magento\Backend\App\Action\Context;
+use Magento\Framework\Flag\FlagResource;
+use Magento\Framework\FlagFactory;
 use Psr\Log\LoggerInterface;
 use Magento\Framework\FlagManager;
 
 abstract class Action extends BackendAction
 {
+    const FLAG_CODE = 'buildateam_customproductbuilder_config';
 
     /**
      * @var LoggerInterface
      */
-    public $logger;
+    protected $logger;
 
     /**
      * @var FlagManager
      */
-    public $flagManager;
+    protected $flagManager;
+
+    /**
+     * @var FlagResource
+     */
+    protected $flagResource;
+
+    /**
+     * @var FlagFactory
+     */
+    protected $flagFactory;
 
     /**
      * Save constructor.
      * @param Context $context
      * @param FlagManager $flagManager
+     * @param FlagResource $flagResource
+     * @param FlagFactory $flagFactory
      * @param LoggerInterface $logger
      */
     public function __construct(
         Context $context,
         FlagManager $flagManager,
+        FlagResource $flagResource,
+        FlagFactory $flagFactory,
         LoggerInterface $logger
     ) {
         parent::__construct($context);
         $this->logger = $logger;
         $this->flagManager = $flagManager;
+        $this->flagResource = $flagResource;
+        $this->flagFactory = $flagFactory;
     }
 }
--- Controller/Adminhtml/Config/Set.php
+++ Controller/Adminhtml/Config/Set.php
@@ -6,10 +6,10 @@ namespace Buildateam\CustomProductBuilder\Controller\Adminhtml\Config;
 
 use Magento\Framework\Controller\Result\Json;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Flag;
 
 class Set extends Action
 {
-
     /**
      * @return Json
      */
@@ -18,10 +18,15 @@ class Set extends Action
         $jsonResult = $this->resultFactory->create(ResultFactory::TYPE_JSON);
 
         try {
-            $this->flagManager->saveFlag(
-                'buildateam_customproductbuilder_config',
-                $this->getRequest()->getParam('config')
+            /** @var Flag $flag */
+            $flag = $this->flagFactory->create(['data' => ['flag_code' => Action::FLAG_CODE]]);
+            $this->flagResource->load(
+                $flag,
+                Action::FLAG_CODE,
+                'flag_code'
             );
+            $flag->setData('flag_data', $this->getRequest()->getParam('config'));
+            $this->flagResource->save($flag);
         } catch (\Exception $e) {
             $this->logger->critical($e->getMessage());
             $resultSave = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
